8d456c3fdf4bdc10d0a4371b487ace9b510a2573
nAtaman <ataman@macpaw.com>
2020-04-26 20:32:00 +0300

    add buttons to fill milk, coffee and water tanks

--- CoffeeMachine/ViewController.swift
+++ CoffeeMachine/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+var coffeMachine = CoffeMachine()
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let coffee = CoffeMachine()
-        coffee.makeAmericano()
+    }
+    
+    @IBAction func fillWaterTankButton(_ sender: Any) {
+        coffeMachine.fillWaterTank()
         
     }
     
+    @IBAction func fillMilkTankButton(_ sender: Any) {
+        coffeMachine.fillMilkTank()
+    }
     
-
+    @IBAction func fillCoffeTankButton(_ sender: Any) {
+        coffeMachine.fillCoffeeTank()
+    }
+    
 
 }
 
